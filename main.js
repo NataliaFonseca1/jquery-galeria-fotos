a67abf77103a74b4e7b5f2e70cefbdba2b322444
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-07-26 14:52:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+
+$("header button").click(function (){
+  $("form").slideDown();
+})
+
+$("#botao-cancelar").click(function(){
+$("form").slideUp();
+
+})
+
+$("form").on("submit", function(e){
+  const newadress=$('#endereco-imagem-nova').val();
+  const novoItem=$('<li style="display: none"></li>')
+  $(`<img src="${newadress}"/>`).appendTo(novoItem);
+  $(`
+  <div class="overlay-imagem-link">
+  <a href="${newadress}" target="_blank" title="Ver imagem em tamanho real">
+  Ver imagem em tamanho real 
+  </a>
+  </div>
+  ` ).appendTo(novoItem);
+$(novoItem).appendTo("ul");
+$(novoItem).fadeIn(1000);
+  $("#endereco-imagem-nova").val(" ")
+  
+  e.preventDefault();
+})
+
+})
+
